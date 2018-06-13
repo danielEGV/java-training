a4e4dd12ec3e8a8ecfba91e771a002204e2dc3e3
danielEGV <deguevarav@unal.edu.co>
2018-06-13 08:59:03 -0500

    ClaseLambda

---
+++ src/main/java/co/com/s4n/training/java/ClaseLambda.java
@@ -0,0 +1,23 @@
+package co.com.s4n.training.java;
+
+import io.vavr.collection.List;
+
+import java.util.function.Consumer;
+
+public class ClaseLambda {
+    String a;
+
+    public Consumer<String> setA() {
+        Consumer<String> c = b -> a = b;
+        return c;
+    }
+
+    public static void main(String[] args) {
+        ClaseLambda claseLambda = new ClaseLambda();
+        Consumer<String> c = claseLambda.setA();
+        c.accept("Hola");
+        System.out.println(claseLambda.a);
+    }
+
+
+}
